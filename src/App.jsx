fd30af550e40e0b1400ed7244f7cf3607f54c63b
Niroop R <niroopparker@gmail.com>
2023-12-10 12:02:22 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+function App() {
+  return (
+    <div className="faq-accordion-card-container">
+      <div className="faq-accordion-card">
+        <div className="faq-accordion-card-header">Header</div>
+        <div className="faq-accordion-card-body">Body</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
